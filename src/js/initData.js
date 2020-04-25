20743e5594dc19e45bb7f9a37e6f416043a7f73d
DarkElectro <electrodark74@gmail.com>
2020-04-25 11:24:58 +0500

    ??

--- src/js/initData.js
+++
@@ -1,7 +0,0 @@
-export default function initData() {
-  return {
-    todo: ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6'],
-    inProgress: ['Task 1', 'Task 2', 'Task 3', 'Task 4'],
-    done: ['Task 1', 'Task 2'],
-  };
-}
